c7a5589b60053dd5f5ed9c22860e247f09d221a0
johngoldsbury <johngoldsbury@hotmail.com>
2014-09-12 23:16:37 +1200

    Added additional command line args to RunCollectionTools and created Params for CaliperBenchmark test RunCollectionTools now has 3 arguments: target directory, DROID sig file path, Jhove output file path CaliperBenchmark params are the same, these are configured from the command line using, for example -Dpath=/pathname/.../...

--- src/com/benchmark/CaliperBenchmark.java
+++ src/com/benchmark/CaliperBenchmark.java
@@ -5,81 +5,83 @@ import uk.gov.nationalarchives.droid.command.action.CommandLineException;
 
 import com.google.caliper.BeforeExperiment;
 import com.google.caliper.Benchmark;
+import com.google.caliper.Param;
 import com.google.caliper.api.Macrobenchmark;
+
 /**
  * Runs all annotated benchmarks using the Google Caliper runner
  * 
  */
 public final class CaliperBenchmark {
 
-	// @Macrobenchmark
-	// public void tika() throws Exception {
-	//
-	// TikaBench tika = new TikaBench();
-	// tika.parseTika("/home/goldsbjohn/govdocs");
-	// // -Dexec.mainClass="com.google.caliper.runner.CaliperMain"
-	// // -Dexec.args="com.benchmark.CaliperBenchmarkTest"
-	// }
+	@Param({ "/home/goldsbjohn/govdocs/" })
+	String path;
+	@Param({ "/home/goldsbjohn/.droid6/signature_files/DROID_SignatureFile_V77.xml" })
+	String sig;
+	@Param({ "/home/goldsbjohn/JhoveResult.txt" })
+	String result;
 
-//	@Macrobenchmark
-//	public void tika_CLI() throws Exception {
-//		TikaCLIBench.parseTikaCLI("/home/goldsbjohn/govdocs/");
-//	}
-//
-//	@Macrobenchmark
-//	public void tika() throws Exception {
-//
-//		TikaBench.parseTika("/home/goldsbjohn/govdocs/");
-//
-//	}
-//
-//	@Macrobenchmark
-//	public void jhoveS_Macro() {
-//		String[] testThing = new String[] { "-c", "conf/jhove.conf", "-s",
-//				"/home/goldsbjohn/govdocs", "-o", "/home/goldsbjohn/test1.txt" };
-//		JhoveCLI.main(testThing);
-//	}
+	// To change any of the above parameters, change command line argument and
+	// add -Dname=/pathname/path/file.ext etc
+	// e.g for maven: ...
+	// -Dexec.args="com.benchmark.CaliperBenchmark -Dpath=/home/goldsbjohn/govdocs"
 
-	// @Benchmark
-	// public void jhoveS_Micro(){
-	// String[] testThing = new String[] { "-c", "conf/jhove.conf", "-s",
-	// "/home/goldsbjohn/govdocs","-o", "/home/goldsbjohn/test1.txt" };
-	// JhoveCLI.main(testThing);
-	// }
-//	@Macrobenchmark
-//	public void jhoveK() {
-//		String[] testThing = new String[] { "-c", "conf/jhove.conf", "-k",
-//				"/home/goldsbjohn/govdocs", "-o", "/home/goldsbjohn/test1.txt" };
-//		JhoveCLI.main(testThing);
-//	}
-//
-//	//
-//	@Macrobenchmark
-//	public void jhoveR() {
-//		String[] testThing = new String[] { "-c", "conf/jhove.conf", "-r",
-//				"/home/goldsbjohn/govdocs", "-o", "/home/goldsbjohn/test1.txt" };
-//		JhoveCLI.main(testThing);
-//	}
+	@Macrobenchmark
+	public void tika_CLI() throws Exception {
+		TikaCLIBench.parseTikaCLI(path);
+	}
 
-	//
-	//
-	
-//	@BeforeExperiment
-//	public void downloadSig() throws CommandLineException {
-//		DroidCLI.main(new String[] { "-d" });
-//	}
+	@Macrobenchmark
+	public void tika() throws Exception {
 
-	@Benchmark
-	public void signatureDownload() throws CommandLineException {
-		DroidCLI.main(new String[] { "-d" });
+		TikaBench.parseTika(path);
+
 	}
 
 //	@Macrobenchmark
-//	public void droid() throws Exception {
-//		String[] testThing = new String[] { "-R", "-Nr",
-//				"/home/goldsbjohn/govdocs", "-Ns",
-//				"/home/goldsbjohn/.droid6/signature_files/DROID_SignatureFile_V77.xml" };
-//		DroidCLI.main(testThing);
+//	public void jhoveS_Macro() {
+//		String[] testThing = new String[] { "-c", "conf/jhove.conf", "-s",
+//				path, "-o", result };
+//		JhoveCLI.main(testThing);
 //	}
+
+//	@Macrobenchmark
+//	public void jhoveK() {
+//		String[] testThing = new String[] { "-c", "conf/jhove.conf", "-k",
+//				path, "-o", result };
+//		JhoveCLI.main(testThing);
+//	}
+
+	
+//	@Macrobenchmark
+//	public void jhoveR() {
+//		String[] testThing = new String[] { "-c", "conf/jhove.conf", "-r",
+//				path, "-o", result };
+//		JhoveCLI.main(testThing);
+//	}
+
+	// @BeforeExperiment
+	// public void downloadSig() throws CommandLineException {
+	// DroidCLI.main(new String[] { "-d" });
+	// }
+	// @Benchmark
+	// public void jhoveS_Micro(){
+	// String[] testThing = new String[] { "-c", "conf/jhove.conf", "-s",
+	// path,"-o", result };
+	// JhoveCLI.main(testThing);
+	// }
+	// @Benchmark
+	// public void signatureDownload() throws CommandLineException {
+	// DroidCLI.main(new String[] { "-d" });
+	//
+	// }
+
+	// @Macrobenchmark
+	// public void droid() throws Exception {
+	// String[] testThing = new String[] { "-R", "-Nr",
+	// path, "-Ns",
+	// sig };
+	// DroidCLI.main(testThing);
+	// }
 
 }
--- src/com/benchmark/RunCollectionTools.java
+++ src/com/benchmark/RunCollectionTools.java
@@ -2,6 +2,8 @@ package com.benchmark;
 
 import java.io.File;
 
+import org.xhtmlrenderer.css.style.Length;
+
 /**
  * Runs Droid, Jhove and Tika from their commandline interfaces
  * 
@@ -19,25 +21,37 @@ public class RunCollectionTools {
 	public static void runTools(String[] args) throws Exception {
 		String[] commandlineArgs = {};
 		String filePath = "";
+		
+		String homeDir = System.getProperty("user.home")+"/";
+		String droidSignaturePath = homeDir+".droid6/signature_files/DROID_SignatureFile_V77.xml";
+		String resultPath = homeDir+"JhoveResult.txt";
 
 		if (args.length > 0) {
 			filePath = new File(args[0]).getAbsolutePath();
-			// System.out.println(fileName);
-		} else {
-			System.out.println("Please enter a valid path");
+			
+		}
+		else {
+			System.out.println("REQUIRED: arg[0] = path to run tools on e.g. /home/{username}/folder or /home/{username}/folder/file.ext \nOPTIONAL: arg[1] = Droid Signature file, default is /home/{username}/.droid6/signature_files/DROID_SignatureFile_V77.xml\nOPTIONAL: arg[2] = results directory and filename for Jhove, default is /home/{username}/JhoveResult.txt");
 			System.exit(0);
 		}
+		if (args.length == 2){
+			droidSignaturePath = new File(args[1]).getAbsolutePath();
+		}
+		if (args.length == 3) {
+			resultPath = new File(args[2]).getAbsolutePath();
+		}
+		
 
 		// Set Droid command-line arguments including directory/file to process
 		// and signature file (for -Nr mode)
 		commandlineArgs = new String[] { "-R", "-Nr", filePath, "-Ns",
-				"/home/goldsbjohn/.droid6/signature_files/DROID_SignatureFile_V77.xml" };
+				droidSignaturePath };
 		// Execute Droid via commandline interface (From within Java)
 		DroidCLI.main(commandlineArgs);
 
 		// Set Jhove commandline arguments
 		commandlineArgs = new String[] { "-c", "conf/jhove.conf", "-s",
-				filePath, "-o", "/home/goldsbjohn/test2.txt" };
+				filePath, "-o", resultPath,  };
 		// Execute Jhove via commandline interface (From within Java)
 		JhoveCLI.main(commandlineArgs);
 
